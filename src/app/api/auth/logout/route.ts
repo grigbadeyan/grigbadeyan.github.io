b4763a3f22e1fef4c284e93e65e5afc5e57cf484
grigbadeyan <grigbadeyan@gmail.com>
2026-04-26 00:33:10 +0400

    delete all

--- src/app/api/auth/logout/route.ts
+++
@@ -1,9 +0,0 @@
-import { NextResponse } from 'next/server'
-import { cookies } from 'next/headers'
-import { COOKIE_NAME } from '@/lib/auth'
-
-export async function POST() {
-  const cookieStore = cookies()
-  cookieStore.delete(COOKIE_NAME)
-  return NextResponse.json({ success: true })
-}
